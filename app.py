37ee566f5a72ff5fdb8262af7434759eb045f37d
AbzalBasha <90755042+Abzalbasha@users.noreply.github.com>
2021-11-13 00:26:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,44 @@
-#!/usr/bin/env python
-from flask import Flask, request
 
-# create app
-app = Flask(_name_)
+# Program make a simple calculator that can add, subtract, multiply and divide using functions
 
+# This function adds two numbers 
+def add(x, y):
+   return x + y
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        # show html form
-        return '''
-            <form method="post">
-                <input type="text" name="expression" />
-                <input type="submit" value="Calculate" />
-            </form>
-        '''
-    elif request.method == 'POST':
-        # calculate result
-        expression = request.form.get('expression')
-        result = eval(expression)
-        return 'result: %s' % result
+# This function subtracts two numbers 
+def subtract(x, y):
+   return x - y
 
-# run app
-if _name_ == '_main_':
-    app.run(debug=True)
+# This function multiplies two numbers
+def multiply(x, y):
+   return x * y
+
+# This function divides two numbers
+def divide(x, y):
+   return x / y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# Take input from the user 
+choice = input("Enter choice(1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
