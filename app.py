bdd8200f3cad864fe338c1fe7fd104b6f85312af
AbzalBasha <90755042+Abzalbasha@users.noreply.github.com>
2021-11-13 01:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,26 @@
-# Program make a simple calculator
+#!/usr/bin/env python
+from flask import Flask, request
 
-# This function adds two numbers
-def add(x, y):
-    return x + y
-
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
+# create app
+app = Flask(_name_)
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        # show html form
+        return '''
+            <form method="post">
+                <input type="text" name="expression" />
+                <input type="submit" value="Calculate" />
+            </form>
+        '''
+    elif request.method == 'POST':
+        # calculate result
+        expression = request.form.get('expression')
+        result = eval(expression)
+        return 'result: %s' % result
 
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-    
-    else:
-        print("Invalid Input")
+# run app
+if _name_ == '_main_':
+    app.run(debug=True)
