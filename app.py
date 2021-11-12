9145b9723046279056f258a8d0ca39e8961c87eb
AbzalBasha <90755042+Abzalbasha@users.noreply.github.com>
2021-11-13 00:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
+# Program make a simple calculator
 
-# Program make a simple calculator that can add, subtract, multiply and divide using functions
-
-# This function adds two numbers 
+# This function adds two numbers
 def add(x, y):
-   return x + y
+    return x + y
 
-# This function subtracts two numbers 
+# This function subtracts two numbers
 def subtract(x, y):
-   return x - y
+    return x - y
 
 # This function multiplies two numbers
 def multiply(x, y):
-   return x * y
+    return x * y
 
 # This function divides two numbers
 def divide(x, y):
-   return x / y
+    return x / y
+
 
 print("Select operation.")
 print("1.Add")
@@ -23,22 +23,32 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-# Take input from the user 
-choice = input("Enter choice(1/2/3/4):")
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
 
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
 
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
 
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
-else:
-   print("Invalid input")
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
+    else:
+        print("Invalid Input")
